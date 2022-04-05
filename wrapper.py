a67c9936aeeb2ff1a14686fb26f057e01553b17f
mglasser16 <mglasser@mines.edu>
2022-04-04 18:28:42 -0600

    Diffusion coefficient

--- wrapper.py
+++ wrapper.py
@@ -21,11 +21,15 @@ test_yaml = 'D:\projects\BatCan\inputs\LiMetal_PorousSep_Air_test.yaml'
 
 #%%
 
-def batcanrun(rate_constant):
-    placeholder = float(rate_constant[0])
+def batcanrun(params):
+    print(params)
+    rate_constant, D_k_li = params
+    placeholder = float(rate_constant)
+    placeholder2 = float(D_k_li)
     with open(test_yaml, 'r') as file:
         imput_file = yaml.safe_load(file)
     imput_file['cathode-surf-reactions'][0]['rate-constant']['A'] = placeholder
+    imput_file['cell-description']['separator']['transport']['diffusion-coefficients'][1]['D_k'] = placeholder2 #D_k lithium
     with open(test_yaml, "w") as f:
         yaml.dump(imput_file, f)
     foldarray =os.listdir(path)
@@ -44,11 +48,11 @@ def batcanrun(rate_constant):
 
 #%%
 test_yaml = 'D:\projects\BatCan\inputs\LiMetal_PorousSep_Air_test.yaml'
-Done = batcanrun([6879000])
+Done = batcanrun([95000], [1.11e-9])
 print(Done)
 
 #%%
-x_start = 5629000.0
+x_start = [85000, 0.5e-9]
 result = spo.minimize(batcanrun, x_start)
 print("x:", result.x)
 print("SSR:", result.fun)
